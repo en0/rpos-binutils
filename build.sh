835690e708b87bee762cc82f32e8de7fa146c012
Ian Laird <irlaird@gmail.com>
2017-08-01 13:18:26 -0600

    Moved environment variables outside of the build.sh script so that the script can be reused.

--- build.sh
+++ build.sh
@@ -1,15 +1,39 @@
 #!/usr/bin/env bash
 
-export SYSROOT="$(readlink -f $(pwd)/../../..)"
-export PREFIX="${SYSROOT}/usr"
-export TARGET=i686-rpos
-export PATH="${PREFIX}/bin:$PATH"
+#export SYSROOT="$(readlink -f $(pwd)/../../..)"
+#export PREFIX="${SYSROOT}/usr"
+#export TARGET=i686-rpos
+#export PATH="${PREFIX}/bin:$PATH"
+
+abort() {
+    MESSAGE=$1
+    EXITCODE=$2
+    if [[ -z "$EXITCODE" ]]
+    then
+        EXITCODE=1
+    fi
+
+    printf "ERROR: %s\n" "${MESSAGE}" 1>&2
+    exit $EXITCODE
+}
+
+require_env() {
+    [[ -z "$2" ]] && \
+        abort "\$$1 is not set. Please source the desired environment." 1
+}
+
+require_env "SYSROOT" "${SYSROOT}"
+require_env "PREFIX" "${PREFIX}"
+require_env "TARGET" "${TARGET}"
+require_env "PATH" "${PATH}"
 
 rm -rf binutils-build
 mkdir binutils-build
 cd binutils-build
 
-../configure --target=$TARGET --prefix=$PREFIX  --with-sysroot=${SYSROOT} --disable-nls --disable-werror
-[ $? == 0 ] && make
-[ $? == 0 ] && make install
-[ $? != 0 ] && exit 2
+../configure \
+    --target=$TARGET \
+    --prefix=$PREFIX \
+    --with-sysroot=${SYSROOT} \
+    --disable-nls \
+    --disable-werror && make && make install
